3f82e8c86816db5ab0b243d1897589150c5df873
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-08-02 23:27:13 +0300

    добавляет Хуки в навигации

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default function App() {
 
   const dispatch = useDispatch();
   useEffect(() => {
+    console.log(dispatch);
     dispatch(authOperations.getCurrentUser());
   }, [dispatch]);
 
--- src/components/UserMenu/Navigation.js
+++ src/components/UserMenu/Navigation.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { authSelectors } from '../../redux/auth';
 
-const Navigation = ({ isAuthenticated }) => (
-    <div>
-        <NavLink to="/" exact> Главная</NavLink>
-        {isAuthenticated && (
-            <NavLink to="/contacts"> Заметки
-            </NavLink>)}
+const styles = {
+    link: {
+        display: 'inline-block',
+        textDecoration: 'none',
+        padding: 12,
+        fontWeight: 700,
+        color: '#2A363B',
+    },
+    activeLink: {
+        color: '#E84A5F',
+    },
+};
 
-    </div>
-)
+export default function Navigation() {
 
-const mapStateToProps = state => ({
-    isAuthenticated: authSelectors.getIsAuthenticated(state),
-})
-export default connect(mapStateToProps)(Navigation);
+    const isLoggedIn = useSelector(authSelectors.getIsAuthenticated);
+
+    return (
+        <div>
+            <NavLink to="/" exact style={styles.link} activeStyle={styles.activeLink}> Главная</NavLink>
+            {isLoggedIn && (
+                <NavLink to="/contacts" style={styles.link} activeStyle={styles.activeLink}> Заметки
+                </NavLink>)}
+
+        </div>
+    )
+}
+
+// const mapStateToProps = state => ({
+//     isAuthenticated: authSelectors.getIsAuthenticated(state),
+// })
+// export default connect(mapStateToProps)(Navigation);
