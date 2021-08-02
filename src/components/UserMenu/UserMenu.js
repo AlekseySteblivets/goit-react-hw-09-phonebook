bcd54ca6db9bba3cab484c628c39da30b2308e63
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-08-03 00:10:03 +0300

    добавляет хуки в UserMenu

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { authSelectors, authOperations } from '../../redux/auth';
 
-const UserMenu = ({ avatar, name, onLogout }) => (
-    <div>
-        <img src={avatar} alt="" width="32" />
-        <span>Welcome, {name}</span>
-        <button type="button" onClick={onLogout}>Выйти</button>
-    </div>
-
-)
-
-const mapStateToProps = state => ({
-    name: authSelectors.getUserName(state),
-    // avatar:
-});
-
-const mapDispatchToProps = {
-    onLogout: authOperations.logout,
+const styles = {
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    avatar: {
+        marginRight: 4,
+    },
+    name: {
+        fontWeight: 700,
+        marginRight: 12,
+    },
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
+export default function UserMenu() {
+    const dispatch = useDispatch();
+    const name = useSelector(authSelectors.getUserName);
+    const onLogout = useCallback(() => { dispatch(authOperations.logout()); }, [dispatch]);
+
+    return (
+        <div style={styles.container}>
+            <img src="" alt="" width="32" style={styles.avatar} />
+            <span style={styles.name}>Welcome, {name}</span>
+            <button type="button" onClick={onLogout}>Выйти</button>
+        </div>
+    )
+}
+
+
+// const mapStateToProps = state => ({
+//     name: authSelectors.getUserName(state),
+//     // avatar:
+// });
+
+// const mapDispatchToProps = {
+//     onLogout: authOperations.logout,
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
