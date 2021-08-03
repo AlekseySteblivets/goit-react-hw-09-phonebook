2b04901a6b432c21e55670a222a8fceb212e76f1
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-08-03 23:24:13 +0300

    Updates

---
+++ static/js/6.248b16a5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-09-phonebooks"]=this["webpackJsonpgoit-react-hw-09-phonebooks"]||[]).push([[6],{85:function(o,e,t){"use strict";t.r(e);t(0);var h=t(1);e.default=function(){return Object(h.jsx)("h1",{children:"Welcome to the phone book!"})}}}]);
+//# sourceMappingURL=6.248b16a5.chunk.js.map
